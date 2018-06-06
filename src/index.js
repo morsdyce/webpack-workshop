dca9990d6a6ce9fa6ba4f2762bd4ee0bcab37e81
Maayan Glikser <maayan@glikm.com>
2018-06-06 21:29:28 +0300

    add non node supported code to fail the build

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import { Index } from './routes';
 
 render('#root', Index);
 
+// this code is here to fail your build if you don't have babel installed
+const a = { a: 1 };
+const b = { b: 2 };
+console.log({ ...a, ...b });
